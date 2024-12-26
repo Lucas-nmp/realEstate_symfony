007b0ae523e0f96539995e3544f3d2b7c1ac37e1
Lucas-nmp <lucas.morandeira.parejo@gmail.com>
2024-12-26 14:09:57 +0100

    add functionality to remove property

--- src/Controller/DeleteController.php
+++ src/Controller/DeleteController.php
@@ -5,14 +5,70 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Property;
 
 class DeleteController extends AbstractController
 {
-    #[Route('/delete', name: 'app_delete')]
-    public function index(): Response
+    #[Route('/delete', name: 'app_delete', methods: ['GET', 'POST'])]
+    public function delete(EntityManagerInterface $entityManager): Response
     {
+
+        $properties = $entityManager->getRepository(Property::class)->findAll();
+
         return $this->render('delete/index.html.twig', [
-            'controller_name' => 'DeleteController',
+            'properties' => $properties,
+            
         ]);
     }
+
+    #[Route('/delete/property', name: 'app_delete_property', methods: ['POST'])]
+    public function deleteProperty(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $id = $request->request->get('property_id');
+
+        $property = $entityManager->getRepository(Property::class)->find($id);
+
+        if (!$property) {
+            $this->addFlash('error', 'Propiedad no encontrada');
+            return $this->redirectToRoute('app_delete');
+        }
+
+        $reference = $property->getReference();
+        $uploadDir = $this->getParameter('kernel.project_dir') . '/public/uploads/' . $reference;
+
+        $entityManager->remove($property);
+        $entityManager->flush();
+
+        if (is_dir($uploadDir)) {
+            $this->deleteDirectory($uploadDir);
+        }
+
+        $this->addFlash('success', 'Propiedad eliminada correctamente!');
+        return $this->redirectToRoute('app_delete');
+    }
+
+    private function deleteDirectory(string $directory): void
+    {
+        if (!is_dir($directory)) {
+            return;
+        }
+
+        $files = array_diff(scandir($directory), ['.', '..']);
+
+        foreach ($files as $file) {
+            $filePath = $directory . DIRECTORY_SEPARATOR . $file;
+
+            if (is_dir($filePath)) {
+                $this->deleteDirectory($filePath); // Llamada recursiva
+            } else {
+                unlink($filePath); // Elimina el archivo
+            }
+        }
+
+        rmdir($directory); // Finalmente elimina la carpeta
+    }
 }
+
--- src/Controller/ModifyController.php
+++ src/Controller/ModifyController.php
@@ -2,17 +2,86 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Property;
 
 class ModifyController extends AbstractController
 {
-    #[Route('/modify', name: 'app_modify')]
-    public function index(): Response
+    #[Route('/modify', name: 'app_modify', methods: ['GET', 'POST'])]
+    public function modify(EntityManagerInterface $entityManager): Response
     {
+
+
+        $properties = $entityManager->getRepository(Property::class)->findAll();
+        
         return $this->render('modify/index.html.twig', [
-            'controller_name' => 'ModifyController',
+            'properties' => $properties,
+            
         ]);
     }
+
+    #[Route('/property/{id}', name: 'app_get_property', methods: ['GET'])]
+    public function getProperty(int $id, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $property = $entityManager->getRepository(Property::class)->find($id);
+
+        if (!$property) {
+            return new JsonResponse(['error' => 'Property not found'], 404);
+        }
+
+        return new JsonResponse([
+            'id' => $property->getId(),
+            'title' => $property->getTitle(),
+            'area' => $property->getArea(),
+            'price' => $property->getPrice(),
+            'reference' => $property->getReference(),
+            'reference' => $property->getReference(),
+            'short_description' => $property->getShortDescription(),
+            'long_description' => $property->getLongDescription(),
+            'outstanding' => $property->isOutstanding(),
+            'operation_type' => $property->getOperationType(),
+            'observation_price' => $property->getPriceObservation(),
+        ]);
+    }
+
+    #[Route('/modify/property', name: 'app_modify_property', methods: ['POST'])]
+    public function modifyProperty(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $id = $request->request->get('property_id');
+        $title = $request->request->get('title');
+        $area = $request->request->get('area');
+        $reference = $request->request->get('reference');
+        $price = $request->request->get('price');
+        $observationPrice = $request->request->get('observation_price');
+        $reference = $request->request->get('reference');
+        $shortDescription = $request->request->get('short_description');
+        $longDescription = $request->request->get('long_description');
+
+        $property = $entityManager->getRepository(Property::class)->find($id);
+
+        if (!$property) {
+            $this->addFlash('error', 'Propiedad no encontrada');
+            return $this->redirectToRoute('app_modify');
+        }
+
+        $property->setTitle($title)
+                ->setArea($area)
+                ->setPrice((float) $price)
+                ->setReference($reference)
+                ->setShortDescription($shortDescription)
+                ->setObservationPrice($observationPrice)
+                ->setLongDescription($longDescription);
+
+        $entityManager->flush();
+
+        $this->addFlash('success', 'Propiedad modificada correctamente!');
+        return $this->redirectToRoute('app_modify');
+    }
+
+
 }

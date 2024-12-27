949895aad113e6dc46a8ecc3c58e6ff179e52cf4
Lucas-nmp <lucas.morandeira.parejo@gmail.com>
2024-12-28 00:50:35 +0100

    Add carousel on property details page

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -6,6 +6,7 @@ use App\Repository\PropertyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Finder\Finder;
 
 class PropertyController extends AbstractController
 {
@@ -17,8 +18,29 @@ class PropertyController extends AbstractController
         if (!$property) {
             throw $this->createNotFoundException("La propiedad no existe.");
         }
+        $reference = $property->getReference();
+
+        $imageDir = 'uploads/' . $reference;
+        $finder = new Finder();
+
+        // Agregar imágenes a cada objeto property
+        if (is_dir($imageDir)) {
+            $finder->files()->in($imageDir)->name('/\\.(jpg|jpeg|png)$/i');
+            $images = [];
+
+            foreach ($finder as $file) {
+                $images[] = $file->getRelativePathname();
+            }
+
+            $property->images = $images;
+        } else {
+            $property->images = [];
+        }
+
+
         return $this->render('property/index.html.twig', [
             'property' => $property, 
+            
         ]);
     }
 }
